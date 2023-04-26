8ba58c5d92aef491fb66ab9a83c74083c95543d4
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-26 16:11:18 +0200

    create array of object for dinamic menu

---
+++ src/assets/data/menuLinks.js
@@ -0,0 +1,26 @@
+export default [
+   {
+      link: 'home',
+      href: '/',
+   },
+   {
+      link: 'about',
+      href: '#',
+   },
+   {
+      link: 'service',
+      href: '#',
+   },
+   {
+      link: 'showcase',
+      href: '#',
+   },
+   {
+      link: 'blog',
+      href: '#',
+   },
+   {
+      link: 'contact',
+      href: '#',
+   },
+]
