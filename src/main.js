c485ff68a632187ba7c5c23c33feb1f1f275fec1
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-26 16:41:47 +0200

    import icon

--- src/main.js
+++ src/main.js
@@ -13,9 +13,10 @@ import { faUserSecret } from '/node_modules/@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { faPinterestP } from '@fortawesome/free-brands-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret, faTwitter, faFacebookF, faPinterestP)
+library.add(faUserSecret, faTwitter, faFacebookF, faPinterestP, faMagnifyingGlass)
 
 createApp(App)
    .component('font-awesome-icon', FontAwesomeIcon)
