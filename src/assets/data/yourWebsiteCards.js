e86a27f8b5775924d53d440e9bfaddfa3d460014
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-27 13:34:44 +0200

    create object for dinamic inputs

---
+++ src/assets/data/yourWebsiteCards.js
@@ -0,0 +1,22 @@
+export default [
+   {
+      title: 'speed optimization',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nesciunt quia laboriosam nam nisi, nemo animi vero magni. Hic, dolorem!',
+      icon: 'src/assets/img/speedometer2.svg',
+   },
+   {
+      title: 'cloud solutions',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nesciunt quia laboriosam nam nisi, nemo animi vero magni. Hic, dolorem!',
+      icon: 'src/assets/img/cloud.svg',
+   },
+   {
+      title: 'web design',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nesciunt quia laboriosam nam nisi, nemo animi vero magni. Hic, dolorem!',
+      icon: 'src/assets/img/tablet.svg',
+   },
+   {
+      title: 'online marketing',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nesciunt quia laboriosam nam nisi, nemo animi vero magni. Hic, dolorem!',
+      icon: 'src/assets/img/stopwatch.svg',
+   },
+]
