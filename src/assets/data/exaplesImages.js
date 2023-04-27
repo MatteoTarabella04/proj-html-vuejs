93ee768fc644e53efe83b96625635b20eca862a9
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-27 19:27:16 +0200

    create card objects

---
+++ src/assets/data/exaplesImages.js
@@ -0,0 +1,42 @@
+export default [
+   {
+      path: 'src/assets/img/ina-soulis-227104-unsplash-1024x1024.jpg',
+      title: 'flowers',
+      category: 'web design'
+   },
+   {
+      path: 'src/assets/img/sunisa-misa-531163-unsplash-1024x1024.jpg',
+      title: 'a damous ferris wheel',
+      category: 'marketing'
+   },
+   {
+      path: 'src/assets/img/355H-1024x1024.jpg',
+      title: 'shoes',
+      category: 'marketing'
+   },
+   {
+      path: 'src/assets/img/photo-1448932252197-d19750584e56-1024x1024.jpg',
+      title: 'desk',
+      category: 'web design'
+   },
+   {
+      path: 'src/assets/img/business-competition-PB366D8-1024x1024.jpg',
+      title: 'business competition',
+      category: 'marketing'
+   },
+   {
+      path: 'src/assets/img/cozy-sofa-in-living-room-PQR5AB9-1024x1024.jpg',
+      title: 'sofa',
+      category: 'web design'
+   },
+   {
+      path: 'src/assets/img/aa9a4539-PQGJ7HU-1024x1024.jpg',
+      title: 'flight baloon',
+      category: 'web design'
+   },
+   {
+      path: 'src/assets/img/cody-davis-253928-unsplash-1024x1024.jpg',
+      title: 'orange fruit',
+      category: 'web design'
+   },
+]
