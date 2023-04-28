294f6de4600904509a761a1f7e1b225ca292b041
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-28 17:40:06 +0200

    create sponsor objects

---
+++ src/assets/data/sponsor.js
@@ -0,0 +1,20 @@
+export default [
+   {
+      path: 'src/assets/img/client-1-2x.png'
+   },
+   {
+      path: 'src/assets/img/client-9-2x.png'
+   },
+   {
+      path: 'src/assets/img/client-7-2x.png'
+   },
+   {
+      path: 'src/assets/img/client-3-2x.png'
+   },
+   {
+      path: 'src/assets/img/client-4-2x.png'
+   },
+   {
+      path: 'src/assets/img/client-5-2x.png'
+   },
+]
